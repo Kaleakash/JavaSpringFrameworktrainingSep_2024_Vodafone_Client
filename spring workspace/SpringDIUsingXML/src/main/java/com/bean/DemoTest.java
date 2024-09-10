201c688830976210d5e2c6f0e325012bfa0193c4
Akash <akash300383@gmail.com>
2024-09-10 17:22:00 +0530

    done

--- spring workspace/SpringDIUsingXML/src/main/java/com/bean/DemoTest.java
+++ spring workspace/SpringDIUsingXML/src/main/java/com/bean/DemoTest.java
@@ -15,17 +15,30 @@ public class DemoTest {
 		
 		Resource res = new ClassPathResource("beans.xml");  // load xml file 	
 		BeanFactory bean = new XmlBeanFactory(res);			// created BeanFactory interface reference
-		Employee e1 = (Employee)bean.getBean("emp1");
-		e1.display();
 		
-		Employee e2 = (Employee)bean.getBean("emp1");
-		e2.display();
-		System.out.println("---------------");
-		Employee e3 = (Employee)bean.getBean("emp2");
-		e3.display();
+//		Employee e1 = (Employee)bean.getBean("emp1");
+//		e1.display();
+//		
+//		Employee e2 = (Employee)bean.getBean("emp1");
+//		e2.display();
+//		System.out.println("---------------");
+//		Employee e3 = (Employee)bean.getBean("emp2");
+//		e3.display();
+//		
+//		Employee e4 = (Employee)bean.getBean("emp2");
+//		e4.display();
 		
-		Employee e4 = (Employee)bean.getBean("emp2");
-		e4.display();
+		Employee e5 = (Employee)bean.getBean("emp1");
+		System.out.println(e5);    // it will call toString method to get details 
+		
+		Employee e6 = (Employee)bean.getBean("emp2");
+		System.out.println(e6);
+		
+		Employee e7 = (Employee)bean.getBean("emp3");
+		System.out.println(e7);
+		
+		Employee e8 = (Employee)bean.getBean("emp4");
+		System.out.println(e8);
 	}
 
 }
--- spring workspace/SpringDIUsingXML/src/main/java/com/bean/Employee.java
+++ spring workspace/SpringDIUsingXML/src/main/java/com/bean/Employee.java
@@ -1,16 +1,48 @@
 package com.bean;
 
 public class Employee {
-
+private int id;
+private String name;
+private float salary;
 	public void display() {
 		System.out.println("This is employee class method");
 	}
-
 	public Employee() {
 		super();
 		// TODO Auto-generated constructor stub
-		System.out.println("object created..");
+		System.out.println("object created..Empty");
 	}
+	public Employee(int id, String name, float salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+		System.out.println("object created using parameter");
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getSalary() {
+		return salary;
+	}
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+
+	
 	
 	
 }
