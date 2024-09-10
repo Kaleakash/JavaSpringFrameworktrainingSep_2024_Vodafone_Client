296a1dcf23efde405e17bf1a64d328bca1be78c9
Akash <akash300383@gmail.com>
2024-09-10 17:02:20 +0530

    done

--- spring workspace/SpringDIUsingXML/src/main/java/com/bean/DemoTest.java
+++ spring workspace/SpringDIUsingXML/src/main/java/com/bean/DemoTest.java
@@ -8,12 +8,24 @@ import org.springframework.core.io.Resource;
 public class DemoTest {
 
 	public static void main(String[] args) {
-		//Employee emp1 = new Employee();
-		//emp1.display();
+//		Employee emp1 = new Employee();
+//		emp1.display();
+//		Employee emp2 = new Employee();
+//		emp2.display();
+		
 		Resource res = new ClassPathResource("beans.xml");  // load xml file 	
 		BeanFactory bean = new XmlBeanFactory(res);			// created BeanFactory interface reference
 		Employee e1 = (Employee)bean.getBean("emp1");
 		e1.display();
+		
+		Employee e2 = (Employee)bean.getBean("emp1");
+		e2.display();
+		System.out.println("---------------");
+		Employee e3 = (Employee)bean.getBean("emp2");
+		e3.display();
+		
+		Employee e4 = (Employee)bean.getBean("emp2");
+		e4.display();
 	}
 
 }
--- spring workspace/SpringDIUsingXML/src/main/java/com/bean/Employee.java
+++ spring workspace/SpringDIUsingXML/src/main/java/com/bean/Employee.java
@@ -5,4 +5,12 @@ public class Employee {
 	public void display() {
 		System.out.println("This is employee class method");
 	}
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+		System.out.println("object created..");
+	}
+	
+	
 }
