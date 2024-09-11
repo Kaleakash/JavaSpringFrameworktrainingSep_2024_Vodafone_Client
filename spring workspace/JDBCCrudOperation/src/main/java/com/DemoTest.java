52f3f7077c4bb5161fada027b92ab4ada27a2b6d
Akash <akash300383@gmail.com>
2024-09-11 15:37:09 +0530

    done

--- spring workspace/JDBCCrudOperation/src/main/java/com/DemoTest.java
+++ spring workspace/JDBCCrudOperation/src/main/java/com/DemoTest.java
@@ -1,5 +1,8 @@
 package com;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DemoTest {
 
 	public static void main(String[] args) {
@@ -8,6 +11,9 @@ public class DemoTest {
 			// load the Driver 	
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			System.out.println("Driver loaded successfully");
+			// Establish the connection 
+	Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/v_db", "root", "root@123");
+	System.out.println("connected db");
 		} catch (Exception e) {
 			System.err.println(e);
 		}
