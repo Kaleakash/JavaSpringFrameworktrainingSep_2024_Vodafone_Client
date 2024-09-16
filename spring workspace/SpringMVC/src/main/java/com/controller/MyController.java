c4338e6220b68850fc72a6f40046a30bee4f04f5
Akash <akash300383@gmail.com>
2024-09-16 14:23:22 +0530

    done

--- spring workspace/SpringMVC/src/main/java/com/controller/MyController.java
+++ spring workspace/SpringMVC/src/main/java/com/controller/MyController.java
@@ -1,15 +1,25 @@
 package com.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MyController {
 
+	@RequestMapping(value = "hello",method = RequestMethod.GET)
 	public ModelAndView sayHello() {
-		System.out.println("I Came Here");
+		System.out.println("I Came Here with path as hello");
 		ModelAndView mav =new ModelAndView();
 		mav.setViewName("display1.jsp");
+		return mav;
+	}
+	@RequestMapping(value = "hi",method = RequestMethod.GET)
+	public ModelAndView sayHi() {
+		System.out.println("I Came Here with path as hi");
+		ModelAndView mav =new ModelAndView();
+		mav.setViewName("display2.jsp");
 		return mav;
 	}
 }
