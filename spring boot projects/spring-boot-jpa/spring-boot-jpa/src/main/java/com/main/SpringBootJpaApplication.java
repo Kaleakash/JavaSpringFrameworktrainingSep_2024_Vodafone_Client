650240505f02b75ca2338bdd615b3d6a0f0efe6b
Akash <akash300383@gmail.com>
2024-09-18 14:46:17 +0530

    done

--- spring boot projects/spring-boot-jpa/spring-boot-jpa/src/main/java/com/main/SpringBootJpaApplication.java
+++ spring boot projects/spring-boot-jpa/spring-boot-jpa/src/main/java/com/main/SpringBootJpaApplication.java
@@ -1,13 +1,16 @@
-package com;
+package com.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com")	// enable @Controller, @Service @Repository 
+@EntityScan(basePackages = "com.entity")	// scan all entity classes. 
 public class SpringBootJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJpaApplication.class, args);
+		System.out.println("spring boot up");
 	}
 
 }
