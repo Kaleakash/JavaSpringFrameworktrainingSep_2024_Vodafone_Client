af5e4e29564da89fa720bf4e3d8b989a97c1147e
Akash <akash300383@gmail.com>
2024-09-19 15:01:44 +0530

    done

---
+++ spring boot projects/spring-boot-restapi/spring-boot-restapi/src/main/java/com/controller/EmployeeController.java
@@ -0,0 +1,57 @@
+
+package com.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bean.Employee;
+
+@RestController
+public class EmployeeController {
+
+	@RequestMapping(value = "employee_json",
+			method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+	public Employee getEmployeeJSON() {
+		Employee emp1 = new Employee(100, "Steven", 45000);
+		return emp1;
+	}
+	
+	@RequestMapping(value = "employee_xml",
+			method = RequestMethod.GET,produces = MediaType.APPLICATION_XML_VALUE)
+	public Employee getEmployeeXML() {
+		Employee emp1 = new Employee(100, "Steven", 45000);
+		return emp1;
+	}
+	@RequestMapping(value = "employees_json",
+			method = RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Employee> getEmployeesJson() {
+		Employee emp1 = new Employee(100, "Steven", 45000);
+		Employee emp2 = new Employee(101, "Ravi", 48000);
+		Employee emp3 = new Employee(102, "Leena", 50000);
+		List<Employee> listOfEmp = new ArrayList<Employee>();
+		listOfEmp.add(emp1);
+		listOfEmp.add(emp2);
+		listOfEmp.add(emp3);
+		return listOfEmp;
+	}
+	// not working with xml list of employee 
+	@RequestMapping(value = "employees_xml",
+			method = RequestMethod.GET,
+			produces = MediaType.APPLICATION_XML_VALUE)
+	public List<Employee> getEmployeesXmlEmployees() {
+		Employee emp1 = new Employee(100, "Steven", 45000);
+		Employee emp2 = new Employee(101, "Ravi", 48000);
+		Employee emp3 = new Employee(102, "Leena", 50000);
+		List<Employee> listOfEmp = new ArrayList<Employee>();
+		listOfEmp.add(emp1);
+		listOfEmp.add(emp2);
+		listOfEmp.add(emp3);
+		return listOfEmp;
+	}
+}
