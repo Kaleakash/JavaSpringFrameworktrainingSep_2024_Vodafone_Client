3d10c70c408f4b960562f9f3f5bfeabf63f5a78b
Akash <akash300383@gmail.com>
2024-09-19 17:04:21 +0530

    done

---
+++ spring boot projects/spring-boot-employee-rest-api/spring-boot-employee-rest-api/src/main/java/com/SpringBootEmployeeRestApiApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEmployeeRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEmployeeRestApiApplication.class, args);
+	}
+
+}
