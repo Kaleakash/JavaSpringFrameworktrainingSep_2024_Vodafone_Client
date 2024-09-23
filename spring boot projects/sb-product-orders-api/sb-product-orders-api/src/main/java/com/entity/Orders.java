7691bb50f41cc7d0b147cfb6931df8132f0618a8
Akash <akash300383@gmail.com>
2024-09-23 14:42:31 +0530

    done

--- spring boot projects/sb-product-orders-api/sb-product-orders-api/src/main/java/com/entity/Orders.java
+++ spring boot projects/sb-product-orders-api/sb-product-orders-api/src/main/java/com/entity/Orders.java
@@ -2,46 +2,32 @@ package com.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
+@Data
+//@Setter
+//@Getter
+//@ToString
+//@AllArgsConstructor
+//@NoArgsConstructor
 public class Orders {
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)		// auto increment 
 private int oid;
 private LocalDateTime orderdatatime;
+@Column
 private int qty;
 private Integer pid;				// FK 	Integer can hold null value
-public int getOid() {
-	return oid;
-}
-public void setOid(int oid) {
-	this.oid = oid;
-}
-public LocalDateTime getOrderdatatime() {
-	return orderdatatime;
-}
-public void setOrderdatatime(LocalDateTime orderdatatime) {
-	this.orderdatatime = orderdatatime;
-}
-public int getQty() {
-	return qty;
-}
-public void setQty(int qty) {
-	this.qty = qty;
-}
-public Integer getPid() {
-	return pid;
-}
-public void setPid(Integer pid) {
-	this.pid = pid;
-}
-@Override
-public String toString() {
-	return "Orders [oid=" + oid + ", orderdatatime=" + orderdatatime + ", qty=" + qty + ", pid=" + pid + "]";
-}
 
 }
--- spring boot projects/sb-product-orders-api/sb-product-orders-api/src/main/java/com/entity/Product.java
+++ spring boot projects/sb-product-orders-api/sb-product-orders-api/src/main/java/com/entity/Product.java
@@ -6,8 +6,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Data
+//@Setter
+//@Getter
 public class Product {
 @Id
 private int pid;
@@ -17,40 +23,5 @@ private int qty;
 @OneToMany
 @JoinColumn(name = "pid")				// make PID as FK in orders table. 
 private List<Orders> orders;
-
-public List<Orders> getOrders() {
-	return orders;
-}
-public void setOrders(List<Orders> orders) {
-	this.orders = orders;
-}
-public int getPid() {
-	return pid;
-}
-public void setPid(int pid) {
-	this.pid = pid;
-}
-public String getPname() {
-	return pname;
-}
-public void setPname(String pname) {
-	this.pname = pname;
-}
-public float getPrice() {
-	return price;
-}
-public void setPrice(float price) {
-	this.price = price;
-}
-public int getQty() {
-	return qty;
-}
-public void setQty(int qty) {
-	this.qty = qty;
-}
-@Override
-public String toString() {
-	return "Product [pid=" + pid + ", pname=" + pname + ", price=" + price + ", qty=" + qty + "]";
-}
 
 }
