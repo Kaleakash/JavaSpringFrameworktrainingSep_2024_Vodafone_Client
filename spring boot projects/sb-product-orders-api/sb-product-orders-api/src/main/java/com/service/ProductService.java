8bc1dd432d23b6e9396b89bab274aa1a913cb20a
Akash <akash300383@gmail.com>
2024-09-23 14:58:19 +0530

    done

---
+++ spring boot projects/sb-product-orders-api/sb-product-orders-api/src/main/java/com/service/ProductService.java
@@ -0,0 +1,62 @@
+package com.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.entity.Product;
+import com.repository.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	ProductRepository productRepository;
+	
+	public String storeProduct(Product product) {
+		if(productRepository.existsById(product.getPid())) {
+			return "Product is must be unique";
+		}else {
+			productRepository.save(product);
+			return "Product informaton stored successfully";
+		}
+	}
+	
+	public String updateProductDetails(Product product) {
+		Optional<Product> result = productRepository.findById(product.getPid());
+		if(result.isPresent()) {
+		Product p	= result.get();
+		p.setPrice(product.getPrice());
+		p.setQty(product.getQty());
+		productRepository.saveAndFlush(p);
+		return "Product details updated successfully";
+		}else {
+			return "Product not present";
+		}
+	}
+	
+	public String deletedProduct(int pid) {
+		Optional<Product> result = productRepository.findById(pid);
+		if(result.isPresent()) {
+			productRepository.deleteById(pid);
+		return "Product deleted successfully";
+		}else {
+			return "Product not present";
+		}
+	}
+	
+	public String findProduct(int pid) {
+		Optional<Product> result = productRepository.findById(pid);
+		if(result.isPresent()) {
+		Product p	= result.get();
+			return p.toString();
+		}else {
+			return "Product not present";
+		}
+	}
+	public List<Product> findAll() {
+		return productRepository.findAll();
+	}
+}
