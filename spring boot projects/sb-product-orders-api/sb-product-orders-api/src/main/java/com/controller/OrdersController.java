4769b3d3b5e6a67eca0194953b8c71bd4e622e0e
Akash <akash300383@gmail.com>
2024-09-23 16:18:56 +0530

    done

---
+++ spring boot projects/sb-product-orders-api/sb-product-orders-api/src/main/java/com/controller/OrdersController.java
@@ -0,0 +1,40 @@
+package com.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.entity.Orders;
+import com.service.OrdersService;
+
+@RestController
+@RequestMapping("orders") // http://localhost:9191/orders
+public class OrdersController {
+
+	@Autowired
+	OrdersService ordersService;
+// curl -X POST http://localhost:9191/orders/place -H "Content-Type:application/json" -d '{"pid":111,"qty":2}'
+	
+	@PostMapping(value = "place",consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String placeOrders(@RequestBody Orders order) {
+		return ordersService.placeOrder(order);
+	}
+	
+	// curl -X GET http://localhost:9191/orders/find
+	@GetMapping(value = "find",produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Orders> findAllOrders() {
+		return ordersService.findAllOrders();
+	}
+	// curl -X GET http://localhost:9191/orders/findbyid/1
+	@GetMapping(value = "findbyid/{oid}",produces = MediaType.APPLICATION_JSON_VALUE)
+	public Orders findAllOrderById(@PathVariable("oid") int oid) {
+		return ordersService.findOrderById(oid);
+	}
+}
---
+++ spring boot projects/sb-product-orders-api/sb-product-orders-api/src/main/java/com/service/OrdersService.java
@@ -0,0 +1,55 @@
+package com.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.entity.Orders;
+import com.entity.Product;
+import com.repository.OrdersRepository;
+import com.repository.ProductRepository;
+
+@Service
+public class OrdersService {
+
+	@Autowired
+	OrdersRepository ordersRepository;
+	
+	@Autowired
+	ProductRepository productRepository;
+	
+	public String placeOrder(Orders order ) {		// pid, qty 
+		Optional<Product> result = productRepository.findById(order.getPid());
+		if(result.isPresent()) {
+			Product p = result.get();
+			p.setQty(p.getQty()-order.getQty());
+			productRepository.saveAndFlush(p);
+					// oid = auto increment 
+				order.setOrderdatatime(LocalDateTime.now());
+			ordersRepository.save(order);
+			return "order placed successfully";
+		}else {
+			return "Order not placed";
+		}
+	}
+	
+	public List<Orders> findAllOrders() {
+		return ordersRepository.findAll();
+	}
+	
+	public Orders findOrderById(int oid) {
+		Optional<Orders> result = ordersRepository.findById(oid);
+		if(result.isPresent()) {
+			Orders o = result.get();
+			return o;
+		}else {
+			return null;
+		}
+	}
+	
+	
+	
+}
