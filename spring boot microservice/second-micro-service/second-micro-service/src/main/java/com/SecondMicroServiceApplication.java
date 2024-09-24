00a966389ec2160d14917ee8d5f7ae06f270d1e4
Akash <akash300383@gmail.com>
2024-09-24 16:14:46 +0530

    done

---
+++ spring boot microservice/second-micro-service/second-micro-service/src/main/java/com/SecondMicroServiceApplication.java
@@ -0,0 +1,16 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication(scanBasePackages = "com")
+@EnableDiscoveryClient
+public class SecondMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondMicroServiceApplication.class, args);
+		System.err.println("second micro service up on port number 8282");
+	}
+
+}
